fb8c670d37873e26dde78566979b7322a11b8f13
sayedariful <sayedariful73@gmail.com>
2023-10-09 15:06:11 +0600

    assignment mmmmmm

--- src/Header/Banner/Banner.jsx
+++ src/Header/Banner/Banner.jsx
@@ -50,20 +50,6 @@ const Banner = () => {
           {/* slid 2 */}
         
       </div>
-      {/* <div className="flex justify-center w-full py-2 gap-2">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-        <a href="#item4" className="btn btn-xs">
-          4
-        </a>
-      </div> */}
     </div>
   );
 };
